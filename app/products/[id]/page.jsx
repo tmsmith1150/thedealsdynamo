761d3aeffdcb5da18b36a0c5a19e764c9f67a836
Michelle Jordan <tmsmith1150@live.com>
2024-10-30 17:13:47 -0400

    the light of the blind you'll see

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,9 +1,36 @@
+import connectDB from "@/config/database";
+import Product from "@/models/Product";
+import ProductHeaderImage from "@/components/ProductHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
 
-const ProductPage = ({ params }) => {
+const ProductPage = async ({ params }) => {
+    await connectDB();
+    const product = await Product.findById(params.id).lean();
     
     return  ( 
-        <div>Product Page {params.id}</div>
+        <>
+        <ProductHeaderImage image={product.images[0]} />
+        {/* <!-- Go Back --> */}
+    <section>
+      <div className="container m-auto py-6 px-6">
+        <Link
+          href="/products"
+          className="text-blue-500 hover:text-blue-600 flex items-center"
+        >
+          <FaArrowLeft className='mr-2' /> Back to Products
+        </Link>
+      </div>
+    </section>
+    <section className="bg-blue-50">
+      <div className="container m-auto py-10 px-6">
+        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+         {/* <!-- Product Info --> */}
+        </div>
+        </div>
+        </section>
+        </>
      );
 }
  
---
+++ components/ProductHeaderImage.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+const ProductHeaderImage = ({ image }) => {
+    return ( 
+        <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <Image
+            src={`/images/products/${image}`}
+            alt=""
+            className="object-cover h-[400px] w-full"
+            width={0}
+            height={0}
+            sizes='100vw'
+          />
+        </div>
+      </div>
+    </section>
+     );
+}
+ 
+export default ProductHeaderImage;
