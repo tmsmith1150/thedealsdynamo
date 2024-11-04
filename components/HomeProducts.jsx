6cb0291f71fcd36709346d1dd0aecfe06b66ad8c
Michelle Jordan <tmsmith1150@live.com>
2024-11-04 14:47:06 -0500

    the walls of gold entomb us

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -1,17 +1,22 @@
 
 import ProductCard from "./ProductCard";
 import connectDB from '@/config/database';
-import products from '@/products.json';
+import Product from "@/models/Product";
 import Link from "next/link";
 
-const HomeProducts = () => {
-    const recentProducts = products.slice(0, 3);
+const HomeProducts = async () => {
+    await connectDB();
+
+    const recentProducts = await Product.find({})
+    .sort({createdAt: -1})
+    .limit(3)
+    .lean();
     return ( 
         <>
         <section className='px-4 py-6'>
             <div className='container-xl lg:container m-auto px04 py-6'>
                 <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">Recent Products</h2>
-                { products.length === 0 ? (
+                { recentProducts.length === 0 ? (
                     <p>No Products Found</p>
                 ) : (
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
