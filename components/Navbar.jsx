9d815100919381cbb85c7ef147c42f8450304f31
Michelle Jordan <tmsmith1150@live.com>
2024-11-17 18:44:39 -0500

    deploy

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = () => {
                             />
 
                             <span className="hidden md:block text-white text-2xl font-bold ml-2"
-                            >The Deals Dynamo</span>
+                            >Deals Dynamo</span>
                         </Link>
                         {/* <!-- Desktop Menu Hidden below md screens --> */}
                         <div className="hidden md:ml-6 md:block">
