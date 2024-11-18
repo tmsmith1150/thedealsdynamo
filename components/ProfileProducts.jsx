7c0d35292b8106a354c7662a033d34927b634abf
Michelle Jordan <tmsmith1150@live.com>
2024-11-17 20:19:45 -0500

    deploy

--- components/ProfileProducts.jsx
+++ components/ProfileProducts.jsx
@@ -5,54 +5,54 @@ import Link from 'next/link';
 import deleteProduct from '@/app/actions/deleteProduct';
 
 
-const ProfileProducts = ({products:initialProducts}) => {
-    const [products, setProducts] = useState(initialProducts)
+const ProfileProducts = ({ products: initialProducts }) => {
+  const [products, setProducts] = useState(initialProducts)
 
-    const handleDeleteProduct = async (productId) => {
-        const confirmed = window.confirm('Are you sure you want to delete this product?')
+  const handleDeleteProduct = async (productId) => {
+    const confirmed = window.confirm('Are you sure you want to delete this product?')
 
-        if(!confirmed) return;
+    if (!confirmed) return;
 
-        await deleteProduct(productId);
+    await deleteProduct(productId);
 
-        const updatedProducts = products.filter((product) => product._id !== productId);
+    const updatedProducts = products.filter((product) => product._id !== productId);
 
-        setProducts(updatedProducts);
-    }
+    setProducts(updatedProducts);
+  }
 
-    return products.map((product) => (
-        <div key={product._id} className="mb-10">
-                <Link href={`/products/${product._id}`}>
-                  <Image
-                    className="h-32 w-full rounded-md object-cover"
-                    src={product.images[0]}
-                    width={1000}
-                    height={200}
-                    alt="Product 1"
-                  />
-                </Link>
-                <div className="mt-2">
-                  <p className="text-lg font-semibold">{product.name}</p>
-                  <p className="text-lg font-semibold">{product.sku}</p>
-                  <p className="text-lg font-semibold">{product.regular}</p>
-                  <p className="text-lg font-semibold">{product.disc}</p>
-                </div>
-                <div className="mt-2">
-                  <Link href={`/products/${product._id}/edit`}
-                    className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                    type="button"
-                    onClick={ () => handleDeleteProduct(product._id)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-    ));
+  return products.map((product) => (
+    <div key={product._id} className="mb-10">
+      <Link href={`/products/${product._id}`}>
+        <Image
+          className="h-32 w-full rounded-md object-cover"
+          src={product.images[0]}
+          width={1000}
+          height={200}
+          alt="Product 1"
+        />
+      </Link>
+      <div className="mt-2">
+        <p className="text-lg font-semibold">{product.name}</p>
+        <p className="text-lg font-semibold">{product.sku}</p>
+        <p className="text-lg font-semibold">{product.regular}</p>
+        <p className="text-lg font-semibold">{product.disc}</p>
+      </div>
+      <div className="mt-2">
+        <Link href={`/products/${product._id}/edit`}
+          className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
+        >
+          Edit
+        </Link>
+        <button
+          className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
+          type="button"
+          onClick={() => handleDeleteProduct(product._id)}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  ));
 }
- 
+
 export default ProfileProducts;
--- context/GlobalContext.js
+++ context/GlobalContext.js
@@ -7,26 +7,26 @@ const GlobalContext = createContext();
 
 //Create Provider
 export function GlobalProvider({ children }) {
-    const[unreadCount, setUnreadCount] = useState(0);
+  const [unreadCount, setUnreadCount] = useState(0);
 
-    const {data: session} = useSession();
+  const { data: session } = useSession();
 
-    useEffect(() => {
-        if (session && session.user) {
-          getUnreadMessageCount().then((res) =>{
-            if(res.count) setUnreadCount(res.count)
-          })  
-        }
-    }, [getUnreadMessageCount, session]);
+  useEffect(() => {
+    if (session && session.user) {
+      getUnreadMessageCount().then((res) => {
+        if (res.count) setUnreadCount(res.count)
+      })
+    }
+  }, [getUnreadMessageCount, session]);
 
-    return(
+  return (
     <GlobalContext.Provider value={{
-        unreadCount,
-        setUnreadCount
-    }}>{ children }</GlobalContext.Provider>);
+      unreadCount,
+      setUnreadCount
+    }}>{children}</GlobalContext.Provider>);
 }
 
 export function useGlobalContext() {
-    return useContext(GlobalContext)
+  return useContext(GlobalContext)
 }
 
